a409eb98143625344a41f5b547ceeed580284c29
kunljha <jha711kunal@gmail.com>
2021-10-07 20:41:09 +0530

    LogItem and Preloader component

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import LogItem from './LogItem'
+import Preloader from '../layout/Preloader'
 
 const Logs = () => {
 	const [logs, setLogs] = useState([])
@@ -18,11 +20,11 @@ const Logs = () => {
 	}, [])
 
 	if (loading) {
-		return <h4>Loading...</h4>
+		return <Preloader />
 	}
 
 	return (
-		<ul className='collection-with-header'>
+		<ul className='collection with-header'>
 			<li className='collection-header'>
 				<h4 className='center'>System Logs</h4>
 			</li>
@@ -30,7 +32,7 @@ const Logs = () => {
 				<p className='center'>No Logs to Show...</p>
 			) : (
 				logs.map((log) => {
-					return <li key={log.id}>{log.message}</li>
+					return <LogItem key={log.id} log={log} />
 				})
 			)}
 		</ul>
