656affe7f9bec5625bb0dd237db181dc3e1a7f23
kunljha <jha711kunal@gmail.com>
2021-10-07 21:19:55 +0530

    AddBtn component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import M from 'materialize-css/dist/js/materialize.min.js'
 import './App.css'
 import SearchBar from './components/layout/SearchBar'
 import Logs from './components/logs/Logs'
+import AddBtn from './components/layout/AddBtn'
 
 const App = () => {
 	useEffect(() => {
@@ -15,6 +16,7 @@ const App = () => {
 		<Fragment>
 			<SearchBar />
 			<div className='container'>
+				<AddBtn />
 				<Logs />
 			</div>
 		</Fragment>
