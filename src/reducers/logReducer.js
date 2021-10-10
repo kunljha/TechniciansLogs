dfac37db4ca069751b7379cda837114760d90c4c
kunljha <jha711kunal@gmail.com>
2021-10-10 22:42:29 +0530

    defined logReducer

---
+++ src/reducers/logReducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+	logs: null,
+	current: null,
+	loading: false,
+	error: null,
+}
+
+const logReducer = (state = initialState, action) => {
+	switch (action.type) {
+		default:
+			return state
+	}
+}
+
+export default logReducer
