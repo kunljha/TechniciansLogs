d1a610b2814a85069a65809728cc6b94558d963b
kunljha <jha711kunal@gmail.com>
2021-10-14 20:16:52 +0530

    defined techReducer & getTechs action

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -46,7 +46,7 @@ export const getLogs = () => async (dispatch) => {
 	} catch (err) {
 		dispatch({
 			type: LOGS_ERROR,
-			payload: err.response.data,
+			payload: err.response.statusText,
 		})
 	}
 }
@@ -72,7 +72,7 @@ export const addLog = (log) => async (dispatch) => {
 	} catch (err) {
 		dispatch({
 			type: LOGS_ERROR,
-			payload: err.response.data,
+			payload: err.response.statusText,
 		})
 	}
 }
@@ -93,7 +93,7 @@ export const deleteLog = (id) => async (dispatch) => {
 	} catch (err) {
 		dispatch({
 			type: LOGS_ERROR,
-			payload: err.response.data,
+			payload: err.response.statusText,
 		})
 	}
 }
@@ -119,7 +119,7 @@ export const updateLog = (log) => async (dispatch) => {
 	} catch (err) {
 		dispatch({
 			type: LOGS_ERROR,
-			payload: err.response.data,
+			payload: err.response.statusText,
 		})
 	}
 }
@@ -139,7 +139,7 @@ export const searchLogs = (text) => async (dispatch) => {
 	} catch (err) {
 		dispatch({
 			type: LOGS_ERROR,
-			payload: err.response.data,
+			payload: err.response.statusText,
 		})
 	}
 }
---
+++ src/reducers/techReducer.js
@@ -0,0 +1,40 @@
+import {
+	GET_TECHS,
+	ADD_TECH,
+	DELETE_TECH,
+	SET_LOADING,
+	TECHS_ERROR,
+} from '../actions/types'
+
+const initialState = {
+	techs: [],
+	loading: false,
+	error: null,
+}
+
+const techReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case GET_TECHS:
+			return {
+				...state,
+				techs: action.payload,
+				loading: false,
+			}
+		case SET_LOADING:
+			return {
+				...state,
+				loading: true,
+			}
+		case TECHS_ERROR:
+			console.error(action.payload)
+			return {
+				...state,
+				error: action.payload,
+			}
+
+		default:
+			return state
+	}
+}
+
+export default techReducer
